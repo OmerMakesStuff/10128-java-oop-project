d50796616f09880ac23aefe5922a1388d51e3fec
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-04-25 22:54:45 +0300

    feat(CollegeMgmt): add generic functions
    Add generic functions for prompting for item name, adding item, and
    listing items; For now all data arrays are strings, this will probably
    change later.

--- src/CollegeMgmt.java
+++ src/CollegeMgmt.java
@@ -4,6 +4,64 @@ import java.util.Scanner;
  * Omer Peled - 209110519
  */
 public class CollegeMgmt {
+  public static String promptForItem(String[] items, Scanner s,
+      String promptMsg, String alreadyExistsMsg) {
+    String newItem;
+    boolean alreadyExists;
+
+    do {
+      System.out.print(promptMsg);
+      newItem = s.nextLine();
+
+      // If array is empty, skip checking if already exists
+      // Since array length always needs to be doubled when resized,
+      // it starts at 1
+      boolean isEmpty = (items.length == 1) && (items[0] == null);
+      if (isEmpty)
+        return newItem;
+
+      alreadyExists = false;
+      for (int i = 0; i < items.length; i++) {
+        if (items[i].equals(newItem))
+          alreadyExists = true;
+      }
+
+      if (alreadyExists)
+        System.err.println(alreadyExistsMsg);
+    } while (alreadyExists);
+
+    return newItem;
+  }
+
+  public static String[] addItem(String[] items, int logicalSize,
+      String newItem) {
+    // If array is too small, double its length and copy existing items
+    if (logicalSize == items.length) {
+      String[] resizedItems = new String[items.length * 2];
+      for (int i = 0; i < items.length; i++) {
+        resizedItems[i] = items[i];
+      }
+      items = resizedItems;
+    }
+
+    items[logicalSize] = newItem;
+    return items;
+  }
+
+  public static void printItems(String[] items) {
+    // Since array length always needs to be doubled when resized,
+    // it starts at 1
+    boolean isEmpty = (items.length == 1) && (items[0] == null);
+    if (isEmpty)
+      System.err.println("None exist.");
+    else {
+      for (int i = 0; i < items.length; i++) {
+        if (items[i] != null)
+          System.out.println(items[i]);
+      }
+    }
+  }
+
   public static void main(String[] args) {
     final String MENU = """
         COLLEGE STAFF MANAGEMENT
