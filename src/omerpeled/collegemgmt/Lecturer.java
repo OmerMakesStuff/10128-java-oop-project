d3ee4b75a9195df487fecdfb4db6a25031af002b
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-05-25 15:56:59 +0300

    feat(Lecturer): add lecturer class

---
+++ src/omerpeled/collegemgmt/Lecturer.java
@@ -0,0 +1,67 @@
+package omerpeled.collegemgmt;
+
+public class Lecturer {
+  public enum Degree {
+    BSC("BSc"), MSC("MSc"), PHD("PhD"), PROF("Prof.");
+
+    final String displayName;
+
+    Degree(String displayName) {
+      this.displayName = displayName;
+    }
+  }
+
+  private String name;
+  private String id;
+  private Degree degree;
+  private String degreeTitle;
+  private double salary;
+  private Department department;
+
+  public Lecturer(
+      String name,
+      String id,
+      Degree degree,
+      String degreeTitle,
+      double salary,
+      Department department) {
+    this.name = name;
+    this.id = id;
+    this.degree = degree;
+    this.degreeTitle = degreeTitle;
+    this.salary = salary;
+    this.department = department;
+    // TODO for main: Leave empty to set this to null
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public Degree getDegree() {
+    return degree;
+  }
+
+  public String getDegreeTitle() {
+    return degreeTitle;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public Department getDepartment() {
+    return department;
+  }
+
+  // TODO: Setters?
+
+  public String toString() {
+    // TODO: toString
+    return super.toString();
+  }
+}
