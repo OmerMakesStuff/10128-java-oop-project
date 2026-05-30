e224db87dd5b5fb569828e2a7928d7e1a905da26
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-05-30 15:36:52 +0300

    feat(Utils): add utils to double array size
    Currently the same util is duplicated with different array types :(
    Later they will probably be merged into a single function with generics.

---
+++ src/omerpeled/collegemgmt/Utils.java
@@ -0,0 +1,29 @@
+package omerpeled.collegemgmt;
+
+public class Utils {
+  // FIXME: CODE DUPLICATION due to different array types :(
+
+  public static Lecturer[] doubleLecturersSize(Lecturer[] original) {
+    Lecturer[] resized = new Lecturer[original.length * 2];
+    for (int i = 0; i < original.length; i++) {
+      resized[i] = original[i];
+    }
+    return resized;
+  }
+
+  public static Committee[] doubleCommitteesSize(Committee[] original) {
+    Committee[] resized = new Committee[original.length * 2];
+    for (int i = 0; i < original.length; i++) {
+      resized[i] = original[i];
+    }
+    return resized;
+  }
+
+  public static Department[] doubleDepartmentsSize(Department[] original) {
+    Department[] resized = new Department[original.length * 2];
+    for (int i = 0; i < original.length; i++) {
+      resized[i] = original[i];
+    }
+    return resized;
+  }
+}
