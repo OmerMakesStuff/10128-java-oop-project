e31c98fb78d11806814396787629d77f3eaf28d7
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-07-13 03:06:52 +0300

    refactor(Committee): remove null safety in member count comparator

--- app/src/main/java/omerpeled/collegemgmt/CommitteeMemberCountComparator.java
+++ app/src/main/java/omerpeled/collegemgmt/CommitteeMemberCountComparator.java
@@ -6,11 +6,6 @@ public class CommitteeMemberCountComparator
     implements Comparator<Committee> {
   @Override
   public int compare(Committee o1, Committee o2) {
-    // Temporarily handle null, will be removed in part 4
-    // TODO: REMOVE
-    if (o1 == null || o2 == null)
-      return 0;
-
     int count1 = o1.getMembers().size();
     int count2 = o2.getMembers().size();
 
